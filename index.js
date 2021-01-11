114ced89efd47cf981df68c427d94d37f84012f3
Himanshu <himanshu121295@gmail.com>
2021-01-10 22:35:15 -0800

    Added Fonts and changed CSS class names

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const methodOverride = require('method-override')
 const path = require('path')
 const ejsMate = require('ejs-mate')
 
-mongoose
-  .connect('mongodb://localhost:27017/toDoList', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('MONGO Connection Open')
-  })
-  .catch((err) => {
-    console.log('ERROR!!!!!')
-    console.log(err)
-  })
+// mongoose
+//   .connect('mongodb://localhost:27017/toDoList', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log('MONGO Connection Open')
+//   })
+//   .catch((err) => {
+//     console.log('ERROR!!!!!')
+//     console.log(err)
+//   })
 
 app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views'))
@@ -31,13 +31,13 @@ app.get('/home', (req, res) => {
   res.render('home')
 })
 app.get('/shop', (req, res) => {
-  res.send('shop')
+  res.render('shop')
 })
 app.get('/story', (req, res) => {
-  res.send('story')
+  res.render('story')
 })
 app.get('/contact', (req, res) => {
-  res.send('contact')
+  res.render('contact')
 })
 
 app.listen(3000, () => {
